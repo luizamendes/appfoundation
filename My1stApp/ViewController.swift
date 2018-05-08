2d78b5efd9e13e99ebccf91a67ceb2d97d8fac3a
Foundation_26 <foundation_26@10-46-49-23.0200.prio.br.iosda.org>
2018-05-08 10:42:11 -0300

    Initial Commit

---
+++ My1stApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My1stApp
+//
+//  Created by Foundation_26 on 08/05/2018.
+//  Copyright © 2018 Foundation_26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
