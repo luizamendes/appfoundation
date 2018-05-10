18351d8831fc9d10bc1f97a432af2a17e52b9d92
Foundation_26 <foundation_26@10-46-40-6.0101.prio.br.iosda.org>
2018-05-10 11:25:00 -0300

    First commit

---
+++ My1stApp/Tela3ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  Tela3ViewController.swift
+//  MeuPrimeiroApp
+//
+//  Created by Luiza Mendes on 08/05/2018.
+//  Copyright © 2018 Luiza Mendes. All rights reserved.
+//
+
+import UIKit
+
+class Tela3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonBackHome(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- My1stApp/ViewController.swift
+++ My1stApp/ViewController.swift
@@ -15,11 +15,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    var colorCounter = 0
+    
+    let colors = [(UIColor.blue, "Azul"),
+                  (UIColor.purple, "Roxo"),
+                  (UIColor.green, "Verde"),
+                  (UIColor.orange, "Laranja")]
+    
+    @IBOutlet weak var switchPage: UISwitch!
+    
+    @IBAction func buttonGo(_ sender: UIButton) {
+        
+        if switchPage.isOn{
+            performSegue(withIdentifier: "paraTela2", sender: self)
+        }else{
+            performSegue(withIdentifier: "paraTela3", sender: self)
+        }
+    }
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        if colorCounter == colors.count {
+            colorCounter = 0
+        }
+        
+        colorLabel.text = colors[colorCounter].1
+        self.view.backgroundColor = colors[colorCounter].0
+        colorCounter += 1
+
     }
 
-
 }
+
 
